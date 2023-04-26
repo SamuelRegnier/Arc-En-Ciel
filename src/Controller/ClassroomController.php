b9cca06ce628013aa3c1efd6f4e8c0844ada7636
Tony <tony.masse2607@gmail.com>
2023-04-26 23:57:14 +0200

    Création d'une requête avec jointure entre la table user et classroom permettant d'afficher la classe liée à l'enseignant via le nom de celui-ci

--- src/Controller/ClassroomController.php
+++ src/Controller/ClassroomController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ClassroomRepository;
+use App\Repository\UserRepository;
 Use Knp\Component\Pager\PaginatorInterface;
 Use Doctrine\ORM\EntityManagerInterface;
 Use App\Entity\Classroom;
@@ -14,10 +15,11 @@ Use App\Form\ClassroomType;
 
 class ClassroomController extends AbstractController
 {
-    #[Route('/classroom/select', name: 'classroom_select')]
-    public function select(ClassroomRepository $repository,
-     PaginatorInterface $paginator,
-      Request $request,
+    #[Route('/classroom/select/', name: 'classroom_select')]
+    public function select(ClassroomRepository $classroomRepository,
+                            UserRepository $userRepository,
+                            PaginatorInterface $paginator,
+                            Request $request,
       ): Response
     {
 
@@ -25,13 +27,32 @@ class ClassroomController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
-        $classrooms = $paginator->paginate(
-            $repository -> findAll(),
-            $request->query->getInt('page', 1), 
-            6
-        );
-        
-        return $this->render('classroom/read.html.twig', ['classrooms' => $classrooms]);
+        $users = $userRepository -> findAll();
+
+        if($request->request->get('enseignant')){
+            $enseignant = $request->request->get('enseignant');
+            $classrooms = $paginator->paginate(
+                $classroom = $classroomRepository -> findByExampleField($enseignant),
+                $request->query->getInt('page', 1), 
+                6
+            );
+            
+
+            return $this->render('classroom/read.html.twig', [
+                'classrooms' => $classrooms,
+                'users' => $users,
+        ]);
+        } else {
+            $classrooms = $paginator->paginate(
+                $classroom = $classroomRepository -> findAll(),
+                $request->query->getInt('page', 1), 
+                6
+            );
+            return $this->render('classroom/read.html.twig', [
+                'classrooms' => $classrooms,
+                'users' => $users,
+        ]);
+        }
     }
 
     #[Route('/classroom/select/{id}', name: 'classroom_select_id')]
