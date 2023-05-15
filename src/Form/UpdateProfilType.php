3f1a09bd181ede9b46340bbb7c4c388492c25e20
Tony <tony.masse2607@gmail.com>
2023-05-15 10:18:45 +0200

    oubli imports

--- src/Form/UpdateProfilType.php
+++ src/Form/UpdateProfilType.php
@@ -6,6 +6,11 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UpdateProfilType extends AbstractType
 {
